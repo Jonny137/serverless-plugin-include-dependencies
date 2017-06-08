18987c6413ccaf13aec67c627913e2d29cc0ae64
Jonas Holtorf <Jonas.Holtorf@gmail.com>
2017-06-08 14:29:02 +0200

    ingnore .webpack for outside dependencies

--- utils.js
+++ utils.js
@@ -5,7 +5,11 @@ const path = require('path');
 module.exports = {
 
   isOutside(base, location) {
-    const relative = path.relative(base, location);
+    let b = base;
+    if(base.indexOf('.webpack') >= 0) {
+      b = base.replace('/.webpack', '');
+    }
+    const relative = path.relative(b, location);
 
     return !!(relative && relative.indexOf('..') !== -1);
   }
